2717f9d73bbf55eb9fb69d694ff5687e2afd41fc
Wallace Mwabini <wm@serv0-wal.rawle.systems>
2019-06-03 03:13:14 +0300

    gPortal v2.0.2

--- includes/common.php
+++
@@ -1,8 +0,0 @@
-<?php
-//variables used to customise brands
-
-$companyname = "gPortal";
-$installurl = "rawle.local";
-$installdir = "gportal-2.0";
-$mailas = "gPortal";
-$logourl = "../includes/logo.png";
